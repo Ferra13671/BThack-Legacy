1420b48b0581a3a870b977279c17b10d841d932d
Ferra <ferra13671@gmail.com>
2025-01-04 13:03:45 +0300

    1.21.1-1.4-beta

---
+++ src/main/java/com/ferra13671/BThack/api/Module/OneActionModule.java
@@ -0,0 +1,69 @@
+package com.ferra13671.BThack.api.Module;
+
+import com.ferra13671.BThack.Core.Client.ModuleList;
+import com.ferra13671.BThack.api.Category.Category;
+import com.ferra13671.BThack.api.Utils.ChatUtils;
+import net.minecraft.util.Formatting;
+
+public class OneActionModule extends Module {
+
+    public OneActionModule(String name, String description, int key, MCategory c, boolean autoEnabled) {
+        super(name, description, key, c, autoEnabled);
+    }
+
+    public OneActionModule(String name, String description, int key, Category c, boolean autoEnabled) {
+        super(name, description, key, c, autoEnabled);
+    }
+
+    @Override
+    public void sendToggleMessage() {
+        if (toggled && ModuleList.chatNotifications.isEnabled() && ModuleList.chatNotifications.moduleToggle.getValue()) {
+            ChatUtils.sendMessage(this.getName() + ": " + Formatting.YELLOW + "Toggled");
+        }
+    }
+
+    @Override
+    public void playOffSound() {
+        //No Action
+    }
+
+    @Override
+    public void toggle() {
+        toggled = !toggled;
+        if (toggled) {
+            sendToggleMessage();
+            playOnSound();
+            onEnable();
+            onDisable();
+            toggled = false;
+        } else {
+            onDisable();
+        }
+    }
+
+    @Override
+    public void setToggled(boolean toggled) {
+        this.toggled = toggled;
+        if (this.toggled) {
+            sendToggleMessage();
+            playOnSound();
+            onEnable();
+            onDisable();
+            this.toggled = false;
+        } else {
+            onDisable();
+        }
+    }
+
+    @Override
+    public void setQuietlyToggled(boolean toggled) {
+        this.toggled = toggled;
+        if (this.toggled) {
+            onEnable();
+            onDisable();
+            this.toggled = false;
+        } else {
+            onDisable();
+        }
+    }
+}
