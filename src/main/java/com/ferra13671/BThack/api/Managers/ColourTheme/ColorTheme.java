b1cd491a24878e200e2d2d3163470d74093de08c
Ferra <ferra13671@gmail.com>
2025-01-13 18:17:21 +0300

    Delete src/main directory

--- src/main/java/com/ferra13671/BThack/api/Managers/ColourTheme/ColorTheme.java
+++
@@ -1,5 +0,0 @@
-package com.ferra13671.BThack.api.Managers.ColourTheme;
-
-public record ColorTheme(String name, int fontColor, int backgroundFontColor, int backgroundFontHoveredColor,
-                         int moduleEnabledColor, int moduleDisabledColor, int arrayListColor) {
-}
--- src/main/java/com/ferra13671/BThack/mixins/gui_and_hud/MixinEntryListWidget.java
+++
@@ -1,9 +0,0 @@
-package com.ferra13671.BThack.mixins.gui_and_hud;
-
-import net.minecraft.client.gui.widget.EntryListWidget;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Deprecated
-@Mixin(EntryListWidget.class)
-public abstract class MixinEntryListWidget {
-}
