60f2bdb7cd89fe0d09901b025da1f1cc50806dd4
Ferra13671 <ferra13671@gmail.com>
2025-02-08 16:37:42 +0300

    Added color setting in AttackTrace

--- src/main/java/com/ferra13671/BThack/impl/Modules/RENDER/AttackTrace.java
+++ src/main/java/com/ferra13671/BThack/impl/Modules/RENDER/AttackTrace.java
@@ -4,6 +4,7 @@ import com.ferra13671.BThack.Core.Render.BThackRender;
 import com.ferra13671.BThack.Core.Render.Line.RenderLine;
 import com.ferra13671.BThack.api.Events.Entity.AttackEntityEvent;
 import com.ferra13671.BThack.api.Events.Render.RenderWorldLastEvent;
+import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.ColorSetting;
 import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.NumberSetting;
 import com.ferra13671.BThack.api.Module.Module;
 import com.ferra13671.BThack.api.Utils.KeyboardUtils;
@@ -11,11 +12,13 @@ import com.ferra13671.BThack.api.Utils.Ticker;
 import com.ferra13671.MegaEvents.Base.EventSubscriber;
 import net.minecraft.entity.Entity;
 
+import java.awt.*;
 import java.util.Arrays;
 
 public class AttackTrace extends Module {
 
     public final NumberSetting renderTime = new NumberSetting("Render Time", this, 20, 5, 50, false);
+    public final ColorSetting color = new ColorSetting("Color", this, new Color(255, 127, 127)).withBlockedAlpha();
 
     public AttackTrace() {
         super("AttackTrace",
@@ -26,7 +29,8 @@ public class AttackTrace extends Module {
         );
 
         initSettings(
-                renderTime
+                renderTime,
+                color
         );
     }
 
@@ -40,7 +44,7 @@ public class AttackTrace extends Module {
         if (attackEntity != null && attackEntity.isAlive()) {
             if (mc.player.distanceTo(attackEntity) < 20) {
                 BThackRender.lineRender.prepareLineRenderer();
-                BThackRender.lineRender.renderLines(Arrays.asList(new RenderLine(attackEntity, 1, 0.5f, 0.5f, 1)));
+                BThackRender.lineRender.renderLines(Arrays.asList(new RenderLine(attackEntity, color.getValue().getRed() / 255f, color.getValue().getGreen() / 255f, color.getValue().getBlue() / 255f, 1)));
                 BThackRender.lineRender.stopLineRenderer();
             }
         }
