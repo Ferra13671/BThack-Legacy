ca00d03337be03b976e9c6b689311a66330a3b10
Ferra13671 <ferra13671@gmail.com>
2025-03-07 11:55:05 +0300

    Modified Tooltips module renderer

--- src/main/java/com/ferra13671/BThack/impl/Modules/RENDER/Tooltips.java
+++ src/main/java/com/ferra13671/BThack/impl/Modules/RENDER/Tooltips.java
@@ -1,11 +1,13 @@
 package com.ferra13671.BThack.impl.Modules.RENDER;
 
 import com.ferra13671.BThack.Core.Render.BThackRender;
+import com.ferra13671.BThack.Core.Render.Font.FontRenderManager;
 import com.ferra13671.BThack.Core.Render.Utils.ColorUtils;
 import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.BooleanSetting;
 import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.ColorSetting;
 import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.NumberSetting;
 import com.ferra13671.BThack.api.Module.Module;
+import com.ferra13671.BThack.api.Shader.Shaders;
 import com.ferra13671.BThack.api.Utils.KeyboardUtils;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
@@ -53,11 +55,14 @@ public class Tooltips extends Module {
 
         BThackRender.guiGraphics.getMatrices().translate(0f, 0f, 600f);
 
-        BThackRender.drawOutlineRect(x + 7, y - 22, x + 159, y + 49, 1, ColorUtils.WHITE);
-        BThackRender.drawRect(x + 8, y - 21, x + 158, y - 6, getFrameColor());
+        BThackRender.drawVerticalGradientOutlineRect(x + 7, y - 22, x + 159, y + 49, 1, ColorUtils.WHITE, ColorUtils.fastRGBA(120, 120, 120, 255));
+        if (frameRainbow.getValue())
+            BThackRender.drawShader(Shaders.INSTANCE.X_RAINBOW, x + 8, y - 21, x + 158, y - 6);
+        else
+            BThackRender.drawRect(x + 8, y - 21, x + 158, y - 6, getFrameColor());
         BThackRender.drawVerticalGradientRect(x + 8, y - 6, x + 158, y + 48, ColorUtils.fastRGBA(5, 5, 5, (int) backGroundAlpha.getValue()), ColorUtils.fastRGBA(50, 50, 50, (int) backGroundAlpha.getValue()));
 
-        BThackRender.drawString(itemStack.getName().getString(), x + 10, y - 18, -1);
+        BThackRender.drawString(itemStack.getName().getString(), x + 10, y - 16, -1, true, FontRenderManager.DrawMode.NORMAL_BOLD);
 
         int slot = 0;
         for (ItemStack stack : stacks) {
@@ -95,10 +100,13 @@ public class Tooltips extends Module {
 
             BThackRender.guiGraphics.getMatrices().translate(0f, 0f, 600f);
             BThackRender.drawVerticalGradientRect(x1, y1 - 10, x2, y2, ColorUtils.fastRGBA(5, 5, 5, (int) backGroundAlpha.getValue()), ColorUtils.fastRGBA(100, 100, 100, (int) backGroundAlpha.getValue()));
-            BThackRender.drawOutlineRect(x1, y1 - 10, x2, y2, 1, getFrameColor()); //yea
+            if (frameRainbow.getValue())
+                BThackRender.drawShaderOutlineRect(Shaders.INSTANCE.X_RAINBOW,x1, y1 - 10, x2, y2, 1);
+            else
+                BThackRender.drawOutlineRect(x1, y1 - 10, x2, y2, 1, getFrameColor()); //yea
 
             BThackRender.guiGraphics.getMatrices().scale(0.75f, 0.75f, 0.75f);
-            BThackRender.drawString(stack.getItem().getName().getString(), (int) ((x1 + 5) * 1.3333), (int) ((y1 - 5) * 1.3333), -1);
+            BThackRender.drawString(stack.getItem().getName().getString(), (int) ((x1 + 5) * 1.3333), (int) ((y1 - 5) * 1.3333), -1, true, FontRenderManager.DrawMode.NORMAL_BOLD);
 
             BThackRender.guiGraphics.getMatrices().pop();
 
@@ -108,7 +116,6 @@ public class Tooltips extends Module {
     }
 
     private int getFrameColor() {
-        if (frameRainbow.getValue()) return ColorUtils.rainbow();
-        else return ColorUtils.fastRGBA(frameColor.getValue().getRed(), frameColor.getValue().getGreen(), frameColor.getValue().getBlue(), 255);
+        return ColorUtils.fastRGBA(frameColor.getValue().getRed(), frameColor.getValue().getGreen(), frameColor.getValue().getBlue(), 255);
     }
 }
