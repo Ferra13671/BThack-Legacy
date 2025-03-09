78bc1ab028fb7ed5c3a8c6e3009bd13475525423
Ferra13671 <ferra13671@gmail.com>
2025-03-09 14:55:20 +0300

    Mixin code changes

--- src/main/java/com/ferra13671/BThack/mixins/entity/MixinClientPlayerEntity.java
+++ src/main/java/com/ferra13671/BThack/mixins/entity/MixinClientPlayerEntity.java
@@ -7,22 +7,19 @@ import com.ferra13671.BThack.api.Managers.Managers;
 import com.ferra13671.BThack.api.Utils.ItemUtils;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.MovementType;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,16 +27,13 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
 
     @Shadow @Final protected MinecraftClient client;
-
-    @Shadow public float prevNauseaIntensity;
-
-    @Shadow public float nauseaIntensity;
 
     @Shadow public Input input;
 
@@ -74,28 +68,10 @@ public abstract class MixinClientPlayerEntity extends AbstractClientPlayerEntity
         return result;
     }
 
-    @Inject(method = "tickNausea", at = @At("HEAD"), cancellable = true)
-    public void modifyUpdateNausea(boolean fromPortalEffect, CallbackInfo ci) {
-        if (ModuleList.portalGod.isEnabled()) {
-            ci.cancel();
-
-            this.prevNauseaIntensity = this.nauseaIntensity;
-            float f = 0.0F;
-            if (fromPortalEffect && this.portalManager != null && this.portalManager.isInPortal()) {
-                if (this.nauseaIntensity == 0.0F) {
-                    this.client.getSoundManager().play(PositionedSoundInstance.ambient(SoundEvents.BLOCK_PORTAL_TRIGGER, this.random.nextFloat() * 0.4F + 0.8F, 0.25F));
-                }
-
-                f = 0.0125F;
-                this.portalManager.setInPortal(false);
-            } else if (this.hasStatusEffect(StatusEffects.NAUSEA) && !this.getStatusEffect(StatusEffects.NAUSEA).isDurationBelow(60)) {
-                f = 0.006666667F;
-            } else if (this.nauseaIntensity > 0.0F) {
-                f = -0.05F;
-            }
-
-            this.nauseaIntensity = MathHelper.clamp(this.nauseaIntensity + f, 0.0F, 1.0F);
-        }
+    @Redirect(method = "tickNausea", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;shouldPause()Z"))
+    public boolean modifyShouldPauseOnTickNausea(Screen instance) {
+        if (ModuleList.portalGod.isEnabled()) return true;
+        else return instance.shouldPause();
     }
 
     @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/tutorial/TutorialManager;onMovement(Lnet/minecraft/client/input/Input;)V"))
