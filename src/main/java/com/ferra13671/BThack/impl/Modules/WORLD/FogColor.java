f72dd5d7c570a8f4ebf8b756fb249c52b1a13515
Ferra13671 <ferra13671@gmail.com>
2025-03-20 21:49:16 +0300

    Added 'Rainbow Speed' setting in FogColor module

--- src/main/java/com/ferra13671/BThack/impl/Modules/WORLD/FogColor.java
+++ src/main/java/com/ferra13671/BThack/impl/Modules/WORLD/FogColor.java
@@ -3,15 +3,19 @@ package com.ferra13671.BThack.impl.Modules.WORLD;
 import com.ferra13671.BThack.Core.Render.Utils.ColorUtils;
 import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.BooleanSetting;
 import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.ColorSetting;
+import com.ferra13671.BThack.api.Managers.managers.Setting.Settings.NumberSetting;
 import com.ferra13671.BThack.api.Module.Module;
+import com.ferra13671.BThack.api.Shader.ShaderTicker;
 import com.ferra13671.BThack.api.Utils.KeyboardUtils;
 import net.minecraft.util.math.Vec3d;
 
 import java.awt.*;
 
 public class FogColor extends Module {
+    private final ShaderTicker ticker = new ShaderTicker();
 
     public final BooleanSetting rainbow = new BooleanSetting("Rainbow", this, false);
+    public final NumberSetting rainbowSpeed = new NumberSetting("Rainbow Speed", this, 1, 0.3, 5, false, rainbow::getValue);
     public final ColorSetting fogColor = new ColorSetting("Fog Color", this, new Color(255, 255, 255), () -> !rainbow.getValue()).withBlockedAlpha();
 
     public final BooleanSetting overworld = new BooleanSetting("Overworld", this, true);
@@ -27,19 +31,26 @@ public class FogColor extends Module {
         );
 
         initSettings(
-                fogColor,
                 rainbow,
+                rainbowSpeed,
+
+                fogColor,
+
                 overworld,
                 nether,
                 end
         );
     }
 
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        ticker.reset();
+    }
+
     public Vec3d getFogColor() {
-        if (rainbow.getValue()) {
-            Color color = new Color(ColorUtils.rainbow());
-            return new Vec3d(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f);
-        } else
-            return new Vec3d(fogColor.getValue().getRed() / 255d, fogColor.getValue().getGreen() / 255d, fogColor.getValue().getBlue() / 255d);
+        ticker.update((float) rainbowSpeed.getValue());
+        Color color = rainbow.getValue() ? new Color(ColorUtils.rainbow(ticker.getPassedTime())) : fogColor.getValue();
+        return new Vec3d(color.getRed() / 255d, color.getGreen() / 255d, color.getBlue() / 255d);
     }
 }
