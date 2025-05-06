ddd170fdbbf1253866c66010d01f0c6ff28a7c94
Ferra13671 <ferra13671@gmail.com>
2025-05-07 00:50:59 +0300

    PacketMine bug fixes

--- src/main/java/com/ferra13671/BThack/impl/Modules/MISC/PacketMine/PacketMine.java
+++ src/main/java/com/ferra13671/BThack/impl/Modules/MISC/PacketMine/PacketMine.java
@@ -24,7 +24,6 @@ import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3i;
 
@@ -44,7 +43,7 @@ public class PacketMine extends Module {
     public final BooleanSetting removeIfUse = new BooleanSetting("Remove If Use", this, true, conveyorMode::getValue).inCategory(generalCategory);
 
     public final BooleanSetting doubleMine = new BooleanSetting("Double Mine", this, false).inCategory(generalCategory);
-    public final NumberSetting doubleSpeed = new NumberSetting("Double Speed", this, 0.85, 0.5, 1, false, doubleMine::getValue).inCategory(generalCategory);
+    public final NumberSetting doubleSpeed = new NumberSetting("Double Speed", this, 1, 0.5, 1, false, doubleMine::getValue).inCategory(generalCategory);
 
     public final BooleanSetting instaRebreak = new BooleanSetting("Insta Rebreak", this, false).inCategory(generalCategory);
 
@@ -95,6 +94,13 @@ public class PacketMine extends Module {
 
     }
 
+    private final List<Vec3i> autoCityVectors = Arrays.asList(
+            new Vec3i(1,0,0),
+            new Vec3i(0,0,1),
+            new Vec3i(-1,0,0),
+            new Vec3i(0,0,-1)
+    );
+
     //Basic variables
     public BreakingBlock currentBreakingBlock;
     private float destroyDelta = 0;
@@ -132,10 +138,8 @@ public class PacketMine extends Module {
         super.onEnable();
 
         clearBreakBlocks();
-        doubleBreakingBlock = null;
         conveyorBlocks.clear();
         currentSlot = -1;
-        breakDelay = breakDelaySet.getValue().intValue();
 
         ModuleList.superInstaMine.setToggled(false);
 
@@ -155,12 +159,9 @@ public class PacketMine extends Module {
         super.onDisable();
 
         clearBreakBlocks();
-        doubleBreakingBlock = null;
         conveyorBlocks.clear();
         if (!nullCheck())
             packetRemoveItem();
-
-        breakDelay = breakDelaySet.getValue().intValue();
 
         ModuleList.treeCutter.setToggled(false);
     }
@@ -171,6 +172,7 @@ public class PacketMine extends Module {
         if (e.getBlockPos() == null) return;
 
         e.setCancelled(true);
+        if (!BlockUtils.canBreak(e.getBlockPos())) return;
         if (currentBreakingBlock != null && currentBreakingBlock.blockPos.equals(e.getBlockPos())) return;
         updateBlock(e.getBlockPos());
     }
@@ -181,37 +183,9 @@ public class PacketMine extends Module {
         if (currentBreakingBlock == null) return;
         if (!removeIfUse.getValue()) return;
         if (conveyorMode.getValue() && conveyorContains(e.getBlockHitResult().getBlockPos())) {
-            conveyorRemove(e.getBlockHitResult().getBlockPos());
+            conveyorBlocks.removeIf(breakingBlock -> breakingBlock.blockPos.equals(e.getBlockHitResult().getBlockPos()));
             e.setCancelled(true);
         }
-    }
-
-    public void updateBlock(BlockPos pos) {
-        if (conveyorMode.getValue()) {
-            if (needAddToConveyor()) {
-                if (!conveyorContains(pos))
-                    conveyorBlocks.add(new BreakingBlock(pos));
-                return;
-            }
-        }
-
-
-        updateBlockInternal(new BreakingBlock(pos));
-    }
-
-    private void updateBlockInternal(BreakingBlock breakingBlock) {
-        if (doubleMine.getValue() && currentBreakingBlock != null)
-            doubleBreakingBlock = currentBreakingBlock;
-        currentBreakingBlock = breakingBlock;
-    }
-
-    private void checkDestroyDelta() {
-        int bestSlot = AutoTool.getBestSlot(mc.world.getBlockState(currentBreakingBlock.blockPos), inventoryMode.getValue() ? 36 : 9);
-        ItemStack stack = mc.player.getInventory().getStack(mc.player.getInventory().selectedSlot);
-        if (bestSlot != -1)
-            stack = mc.player.getInventory().getStack(bestSlot);
-
-        destroyDelta = ItemUtils.getMineSpeed(mc.world.getBlockState(currentBreakingBlock.blockPos), currentBreakingBlock.blockPos, stack);
     }
 
     @EventSubscriber
@@ -248,19 +222,19 @@ public class PacketMine extends Module {
                     color[3] * 0.3f));
         }
 
-        if (currentBreakingBlock != null) {
-            if (conveyorAnimation.getEase() >= 1) {
-                conveyorAnimation.reset();
-                conveyorAnimationInvert = !conveyorAnimationInvert;
-            }
-
-            renderBoxes.add(getRenderBox(currentBreakingBlock));
+        if (getAnyBreakingBlock() != null) {
+            if (currentBreakingBlock != null)
+                renderBoxes.add(getRenderBox(currentBreakingBlock));
 
             if (doubleMine.getValue() && doubleBreakingBlock != null)
-                if (BlockUtils.canBreak(doubleBreakingBlock.blockPos))
-                    renderBoxes.add(getRenderBox(doubleBreakingBlock));
+                renderBoxes.add(getRenderBox(doubleBreakingBlock));
 
             if (conveyorMode.getValue()) {
+                if (conveyorAnimation.getEase() >= 1) {
+                    conveyorAnimation.reset();
+                    conveyorAnimationInvert = !conveyorAnimationInvert;
+                }
+
                 float animStep = (float) (conveyorAnimationInvert ? 1 - conveyorAnimation.getEase() : conveyorAnimation.getEase());
                 float[] convColor = new float[]{conveyorColor.getValue().getRed() / 255f, conveyorColor.getValue().getGreen() / 255f, conveyorColor.getValue().getBlue() / 255f, conveyorColor.getValue().getAlpha() / 255f};
                 float convBoxSize = conveyorBoxSize.getValue().floatValue() / 2f;
@@ -273,10 +247,9 @@ public class PacketMine extends Module {
                         convColor[3] *= animStep;
                     }
                 }
-                float conveyorBoxAlpha = 0.3f * convColor[3];
                 for (BreakingBlock pos : conveyorBlocks) {
                     renderBoxes.add(
-                            createRenderBox(BlockUtils.createBox(
+                            new RenderBox(BlockUtils.createBox(
                                             pos.blockPos,
                                             convBoxSize,
                                             convBoxSize,
@@ -287,7 +260,10 @@ public class PacketMine extends Module {
                                     convColor[1],
                                     convColor[2],
                                     convColor[3],
-                                    conveyorBoxAlpha
+                                    convColor[0],
+                                    convColor[1],
+                                    convColor[2],
+                                    0.3f * convColor[3]
                             )
                     );
                 }
@@ -301,8 +277,57 @@ public class PacketMine extends Module {
         }
     }
 
+    @EventSubscriber
+    public void onTick(ClientTickEvent e) {
+        if (nullCheck() || mc.isPaused()) return;
+
+        if (instaRebreak.getValue() && breakedPos != null) {
+            if (BlockUtils.canBreak(breakedPos) && currentBreakingBlock == null) {
+                updateBlock(breakedPos);
+                breakDelay = 0; //For InstaRebreak, the break delay doesn't make sense
+            }
+        }
+
+        if (!conveyorMode.getValue() && !conveyorBlocks.isEmpty())
+            conveyorBlocks.clear();
+
+        if (autoCityMode.getValue())
+            autoCityAction();
+
+
+        if (getAnyBreakingBlock() == null) {
+            destroyDelta = 0;
+            return;
+        }
+
+        if (destroyDelta == 0)
+            checkDestroyDelta();
+
+        if (breakDelay > 0) {
+            breakDelay--;
+            return;
+        }
+        if (!updateBreakProgress()) {
+            packetRemoveItem();
+
+            clearBreakBlocks();
+            if (conveyorMode.getValue()) {
+                conveyorBlocks.removeIf(breakingBlock -> breakingBlock.equals(currentBreakingBlock) || breakingBlock.equals(doubleBreakingBlock));
+
+                if (!conveyorBlocks.isEmpty()) {
+                    updateBlock(conveyorBlocks.getFirst().blockPos);
+                    conveyorBlocks.removeFirst();
+                    if (doubleMine.getValue() && !conveyorBlocks.isEmpty()) {
+                        updateBlock(conveyorBlocks.getFirst().blockPos);
+                        conveyorBlocks.removeFirst();
+                    }
+                }
+            }
+        }
+    }
+
     private RenderBox getRenderBox(BreakingBlock breakingBlock) {
-        double blockSize = createLerpSize(breakingBlock);
+        double blockSize = MathHelper.lerp(mc.getRenderTickCounter().getTickDelta(true), breakingBlock.prevDestroyProgress, breakingBlock.currentDestroyProgress) / 2;
         float boxR = (float) (boxMode.getValue().equals("Static Color") ? boxColor.getValue().getRed() / 255f : MathHelper.lerp(breakingBlock.currentDestroyProgress, startBoxColor.getValue().getRed(), endBoxColor.getValue().getRed()) / 255f);
         float boxG = (float) (boxMode.getValue().equals("Static Color") ? boxColor.getValue().getGreen() / 255f : MathHelper.lerp(breakingBlock.currentDestroyProgress, startBoxColor.getValue().getGreen(), endBoxColor.getValue().getGreen()) / 255f);
         float boxB = (float) (boxMode.getValue().equals("Static Color") ? boxColor.getValue().getBlue() / 255f : MathHelper.lerp(breakingBlock.currentDestroyProgress, startBoxColor.getValue().getBlue(), endBoxColor.getValue().getBlue()) / 255f);
@@ -325,94 +350,56 @@ public class PacketMine extends Module {
         );
     }
 
-    private RenderBox createRenderBox(Box box, float red, float green, float blue, float linesAlpha, float boxAlpha) {
-        return new RenderBox(box, red, green, blue, linesAlpha, red, green, blue, boxAlpha);
-    }
-
-    private double createLerpSize(BreakingBlock breakingBlock) {
-        return MathHelper.lerp(mc.getRenderTickCounter().getTickDelta(true), breakingBlock.prevDestroyProgress, breakingBlock.currentDestroyProgress) / 2;
-    }
-
-    @EventSubscriber
-    public void onTick(ClientTickEvent e) {
-        if (nullCheck() || mc.isPaused()) return;
-
-        if (instaRebreak.getValue() && breakedPos != null) {
-            if (!mc.world.isAir(breakedPos) && BlockUtils.canBreak(breakedPos) && currentBreakingBlock == null) {
-                updateBlock(breakedPos);
-                breakDelay = 0; //For InstaRebreak, the break delay doesn't make sense
-            }
-        }
-
-        if ((!conveyorMode.getValue() || conveyorBlocks.isEmpty()) && currentBreakingBlock == null) doubleBreakingBlock = null;
-
-        if (!conveyorMode.getValue() && !conveyorBlocks.isEmpty())
-            conveyorBlocks.clear();
-
-        if (autoCityMode.getValue())
-            autoCityAction();
-
-
-        if (currentBreakingBlock == null && (!doubleMine.getValue() || doubleBreakingBlock == null)) {
-            destroyDelta = 0;
+    public void updateBlock(BlockPos pos) {
+        if (conveyorMode.getValue() && needAddToConveyor()) {
+            if (!conveyorContains(pos))
+                conveyorBlocks.add(new BreakingBlock(pos));
             return;
         }
 
-        if (destroyDelta == 0)
-            checkDestroyDelta();
+        if (doubleMine.getValue() && currentBreakingBlock != null)
+            doubleBreakingBlock = currentBreakingBlock;
+        currentBreakingBlock = new BreakingBlock(pos);
+    }
 
-        if (breakDelay > 0) {
-            breakDelay--;
-            return;
-        }
-        if (!updateBreakProgress()) {
-            packetRemoveItem();
+    private void checkDestroyDelta() {
+        BreakingBlock breakingBlock = getAnyBreakingBlock();
+        int bestSlot = AutoTool.getBestSlot(mc.world.getBlockState(breakingBlock.blockPos), inventoryMode.getValue() ? 36 : 9);
+        ItemStack stack = mc.player.getInventory().getStack(mc.player.getInventory().selectedSlot);
+        if (bestSlot != -1)
+            stack = mc.player.getInventory().getStack(bestSlot);
 
-            if (conveyorMode.getValue()) {
-                conveyorBlocks.removeIf(breakingBlock -> breakingBlock.equals(currentBreakingBlock) || breakingBlock.equals(doubleBreakingBlock));
-
-                breakedPos = currentBreakingBlock.blockPos;
-                clearBreakBlocks();
-                if (!conveyorBlocks.isEmpty()) {
-                    updateBlock(conveyorBlocks.getFirst().blockPos);
-                    conveyorBlocks.removeFirst();
-                    if (doubleMine.getValue() && !conveyorBlocks.isEmpty()) {
-                        updateBlock(conveyorBlocks.getFirst().blockPos);
-                        conveyorBlocks.removeFirst();
-                    }
-                }
-            } else {
-                breakedPos = currentBreakingBlock.blockPos;
-                clearBreakBlocks();
-            }
-        }
+        destroyDelta = ItemUtils.getMineSpeed(mc.world.getBlockState(breakingBlock.blockPos), breakingBlock.blockPos, stack);
     }
 
     public boolean updateBreakProgress() {
-        if (currentBreakingBlock == null) return false;
-        if (!currentBreakingBlock.canBreak() && (doubleBreakingBlock == null || (!doubleMine.getValue() || !doubleBreakingBlock.canBreak()))) {
+        if (getAnyBreakingBlock() == null) return false;
+        if ((currentBreakingBlock == null || !currentBreakingBlock.canBreak()) && (doubleBreakingBlock == null || (!doubleMine.getValue() || !doubleBreakingBlock.canBreak()))) {
             packetRemoveItem();
             destroyDelta = 0;
             return false;
         }
         if (currentSlot == -1) packetEquipItem();
 
-        if (checkStopAction(currentBreakingBlock)) return false;
-        if (doubleMine.getValue() && doubleBreakingBlock != null)
-            checkStopAction(doubleBreakingBlock);
+        boolean currentStop = checkStopAction(currentBreakingBlock);
+        boolean doubleStop = !doubleMine.getValue() || checkStopAction(doubleBreakingBlock);
+        if (currentStop && doubleStop) return false;
 
         updateBreakProgressInternal(currentBreakingBlock);
-        if (doubleMine.getValue() && doubleBreakingBlock != null)
+        if (doubleMine.getValue())
             updateBreakProgressInternal(doubleBreakingBlock);
 
-        if (!currentBreakingBlock.canBreak())
+        if (currentBreakingBlock != null && !currentBreakingBlock.canBreak()) {
+            breakedPos = currentBreakingBlock.blockPos;
             currentBreakingBlock = null;
+        }
         if (doubleBreakingBlock != null && !doubleBreakingBlock.canBreak())
             doubleBreakingBlock = null;
         return true;
     }
 
     public boolean checkStopAction(BreakingBlock breakingBlock) {
+        if (breakingBlock == null) return true;
         if (breakingBlock.currentDestroyProgress == 1) {
             if (breakingBlock == currentBreakingBlock)
                 if (instaRebreak.getValue() && breakedPos != null && breakedPos.equals(currentBreakingBlock.blockPos)) return false;
@@ -426,6 +413,7 @@ public class PacketMine extends Module {
     }
 
     public void updateBreakProgressInternal(BreakingBlock breakingBlock) {
+        if (breakingBlock == null) return;
         if (!breakingBlock.startDestroying) {
             if (breakingBlock == currentBreakingBlock)
                 if (doubleMine.getValue() && doubleBreakingBlock != null && !doubleBreakingBlock.startDestroying) return;
@@ -459,38 +447,39 @@ public class PacketMine extends Module {
         Managers.NETWORK_MANAGER.sendSequencePacket(id -> new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, blockPos, RotateUtils.getInvertedFacingEntity(mc.player), id));
     }
 
-    private final List<Vec3i> autoCityVectors = Arrays.asList(
-            new Vec3i(1,0,0),
-            new Vec3i(0,0,1),
-            new Vec3i(-1,0,0),
-            new Vec3i(0,0,-1)
-    );
     private void autoCityAction() {
         for (PlayerEntity player : mc.world.getPlayers()) {
-            if (player == mc.player) continue;
-            if (!friends.getValue() && Managers.FRIENDS_MANAGER.contains(player)) continue;
-            if (player.distanceTo(mc.player) > 4) continue;
+            if (player == mc.player ||
+                    (!friends.getValue() && Managers.FRIENDS_MANAGER.contains(player)) ||
+                    player.distanceTo(mc.player) > 4
+            ) continue;
             BlockPos blockPos = BlockPos.ofFloored(player.getX(), player.getY(),player.getZ());
             if (BlockUtils.canBreak(blockPos) && MathUtils.getDistance(mc.player.getPos(), blockPos.toCenterPos()) < 4.25)
                 updateBlock(blockPos);
-            BlockPos nearestPos = null;
-            double nearestLength = 9999;
-            for (Vec3i vec : autoCityVectors) {
-                BlockPos checkPos = BlockPos.ofFloored(player.getX() + vec.getX(), player.getY(), player.getZ() + vec.getZ());
-                double length = MathUtils.getDistance(mc.player.getPos(), checkPos.toCenterPos());
-                if (length > 4.25) continue;
-                if (length < nearestLength) {
-                    nearestLength = length;
-                    nearestPos = checkPos;
-                }
-            }
-            if (nearestPos == null) return;
-            if (BlockUtils.canBreak(nearestPos)) updateBlock(nearestPos);
+
+            BlockPos nearestPos = getNearestAutoCityBlockPos(player);
+            if (nearestPos != null && BlockUtils.canBreak(nearestPos)) updateBlock(nearestPos);
         }
     }
 
+    private BlockPos getNearestAutoCityBlockPos(PlayerEntity player) {
+        BlockPos nearestPos = null;
+        double nearestLength = -1;
+        for (Vec3i vec : autoCityVectors) {
+            BlockPos checkPos = BlockPos.ofFloored(player.getX() + vec.getX(), player.getY(), player.getZ() + vec.getZ());
+            double length = MathUtils.getDistance(mc.player.getPos(), checkPos.toCenterPos());
+            if (length > 4.25) continue;
+            if (length < nearestLength || nearestLength < 0) {
+                nearestLength = length;
+                nearestPos = checkPos;
+            }
+        }
+        return nearestPos;
+    }
+
     private void packetEquipItem() {
-        int bestSlot = AutoTool.getBestSlot(mc.world.getBlockState(currentBreakingBlock.blockPos), inventoryMode.getValue() ? 36 : 9);
+        BreakingBlock breakingBlock = getAnyBreakingBlock();
+        int bestSlot = AutoTool.getBestSlot(mc.world.getBlockState(breakingBlock.blockPos), inventoryMode.getValue() ? 36 : 9);
         if (bestSlot != -1) {
             if (bestSlot < 9) {
                 Managers.NETWORK_MANAGER.sendPacket(new UpdateSelectedSlotC2SPacket(bestSlot));
@@ -519,15 +508,6 @@ public class PacketMine extends Module {
         return false;
     }
 
-    private void conveyorRemove(BlockPos pos) {
-        for (BreakingBlock breakingBlock : conveyorBlocks) {
-            if (breakingBlock.blockPos.equals(pos)) {
-                conveyorBlocks.remove(breakingBlock);
-                return;
-            }
-        }
-    }
-
     public void clearBreakBlocks() {
         currentBreakingBlock = null;
         doubleBreakingBlock = null;
@@ -536,5 +516,9 @@ public class PacketMine extends Module {
 
     public boolean needAddToConveyor() {
         return currentBreakingBlock != null && (!doubleMine.getValue() || doubleBreakingBlock != null);
+    }
+
+    public BreakingBlock getAnyBreakingBlock() {
+        return (currentBreakingBlock == null && doubleMine.getValue()) ? doubleBreakingBlock : currentBreakingBlock;
     }
 }
